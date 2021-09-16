93b78250e027582db9c112053209973ad3e0ba1f
KorostelevMK <korostelevmk@mail.ru>
2021-09-16 10:09:18 +0300

    Calculator1

--- CalSwiftUi/ContentView.swift
+++ CalSwiftUi/ContentView.swift
@@ -9,8 +9,325 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Color("BacCol")
+            //--
+            VStack(spacing: 1){
+                Spacer()
+                Text("0")
+                    .foregroundColor(
+                    .white)
+                    .font(
+                    .system(size: 80))
+                    .fontWeight(.ultraLight)
+                    .frame(width: UIScreen.main.bounds.width, alignment: .trailing)
+                
+               //---Гор стек 1
+                HStack(spacing: 1) {
+                    
+                    //---Button
+                    Button(action:{
+                    }, label: {
+                        Text("AC")
+                            .foregroundColor(.white)
+                            .font(.system(size: 40 ))
+                            .fontWeight(.ultraLight)
+                            .frame(width:
+                                UIScreen.main.bounds.width/4, height:UIScreen.main.bounds.width/4, alignment: .center).background(Color ("DarkSilver")
+                        
+                        )
+                    
+                    })
+                    
+  
+                    Button(action:{
+                    }, label: {
+                        Text("+/-")
+                     //   Image(systemName: "devide")
+                            .foregroundColor(.white)
+                            .font(.system(size: 40 ))
+                            .fontWeight(.ultraLight)
+                            .frame(width:
+                                UIScreen.main.bounds.width/4, height:UIScreen.main.bounds.width/4, alignment: .center).background(Color ("DarkSilver")
+                        
+                        )
+                    
+                    })
+                    
+                    
+                    Button(action:{
+                    }, label: {
+                        Text("%")
+                            .foregroundColor(.white)
+                            .font(.system(size: 40 ))
+                            .fontWeight(.ultraLight)
+                            .frame(width:
+                                UIScreen.main.bounds.width/4, height:UIScreen.main.bounds.width/4, alignment: .center).background(Color ("DarkSilver")
+                        
+                        )
+                    
+                    })
+                    
+                    Button(action:{
+                    }, label: {
+                        Image(systemName: "divide")
+                            //Image(system: "plus.slash.minus")
+                            .foregroundColor(.white)
+                            .font(.system(size: 40, weight: .ultraLight, design: .default))
+                            .frame(width:
+                                UIScreen.main.bounds.width/4, height:UIScreen.main.bounds.width/4, alignment: .center)
+                            .background(Color ("OrangCol")
+                        
+                        )
+                    
+                    })
+                    //--конец ряда
+                }
+                // --
+                
+                //---Гор стек 2
+                 HStack(spacing: 1) {
+                     
+                     //---Button
+                     Button(action:{
+                     }, label: {
+                         Text("7")
+                             .foregroundColor(.white)
+                             .font(.system(size: 40 ))
+                             .fontWeight(.ultraLight)
+                             .frame(width:
+                                 UIScreen.main.bounds.width/4, height:UIScreen.main.bounds.width/4, alignment: .center).background(Color ("SilverCol")
+                         
+                         )
+                     
+                     })
+                     
+   
+                     Button(action:{
+                     }, label: {
+                         Text("8")
+                      //   Image(systemName: "devide")
+                             .foregroundColor(.white)
+                             .font(.system(size: 40 ))
+                             .fontWeight(.ultraLight)
+                             .frame(width:
+                                 UIScreen.main.bounds.width/4, height:UIScreen.main.bounds.width/4, alignment: .center).background(Color ("SilverCol")
+                         
+                         )
+                     
+                     })
+                     
+                     
+                     Button(action:{
+                     }, label: {
+                         Text("9")
+                             .foregroundColor(.white)
+                             .font(.system(size: 40 ))
+                             .fontWeight(.ultraLight)
+                             .frame(width:
+                                 UIScreen.main.bounds.width/4, height:UIScreen.main.bounds.width/4, alignment: .center).background(Color ("SilverCol")
+                         
+                         )
+                     
+                     })
+                     
+                    
+                    Button(action:{
+                    }, label: {
+                        Text("X")
+                            .foregroundColor(.white)
+                            .font(.system(size: 40 ))
+                            .fontWeight(.ultraLight)
+                            .frame(width:
+                                UIScreen.main.bounds.width/4, height:UIScreen.main.bounds.width/4, alignment: .center).background(Color ("OrangCol")
+                        
+                        )
+                    
+                    })
+                    
+                     //--конец ряда
+                 }
+                 // --
+                
+                //---Гор стек 3
+                 HStack(spacing: 1) {
+                     
+                     //---Button
+                     Button(action:{
+                     }, label: {
+                         Text("4")
+                             .foregroundColor(.white)
+                             .font(.system(size: 40 ))
+                             .fontWeight(.ultraLight)
+                             .frame(width:
+                                 UIScreen.main.bounds.width/4, height:UIScreen.main.bounds.width/4, alignment: .center).background(Color ("SilverCol")
+                         
+                         )
+                     
+                     })
+                     
+   
+                     Button(action:{
+                     }, label: {
+                         Text("5")
+                      //   Image(systemName: "devide")
+                             .foregroundColor(.white)
+                             .font(.system(size: 40 ))
+                             .fontWeight(.ultraLight)
+                             .frame(width:
+                                 UIScreen.main.bounds.width/4, height:UIScreen.main.bounds.width/4, alignment: .center).background(Color ("SilverCol")
+                         
+                         )
+                     
+                     })
+                     
+                     
+                     Button(action:{
+                     }, label: {
+                         Text("6")
+                             .foregroundColor(.white)
+                             .font(.system(size: 40 ))
+                             .fontWeight(.ultraLight)
+                             .frame(width:
+                                 UIScreen.main.bounds.width/4, height:UIScreen.main.bounds.width/4, alignment: .center).background(Color ("SilverCol")
+                         
+                         )
+                     
+                     })
+                    
+                    Button(action:{
+                    }, label: {
+                        Text("-")
+                            .foregroundColor(.white)
+                            .font(.system(size: 40 ))
+                            .fontWeight(.ultraLight)
+                            .frame(width:
+                                UIScreen.main.bounds.width/4, height:UIScreen.main.bounds.width/4, alignment: .center).background(Color ("OrangCol")
+                        
+                        )
+                    
+                    })
+                     //--конец ряда
+                 }
+                 // --
+                
+                //---Гор стек 4
+                 HStack(spacing: 1) {
+                     
+                     //---Button
+                     Button(action:{
+                     }, label: {
+                         Text("1")
+                             .foregroundColor(.white)
+                             .font(.system(size: 40 ))
+                             .fontWeight(.ultraLight)
+                             .frame(width:
+                                 UIScreen.main.bounds.width/4, height:UIScreen.main.bounds.width/4, alignment: .center).background(Color ("SilverCol")
+                         
+                         )
+                     
+                     })
+                     
+   
+                     Button(action:{
+                     }, label: {
+                         Text("2")
+                      //   Image(systemName: "devide")
+                             .foregroundColor(.white)
+                             .font(.system(size: 40 ))
+                             .fontWeight(.ultraLight)
+                             .frame(width:
+                                 UIScreen.main.bounds.width/4, height:UIScreen.main.bounds.width/4, alignment: .center).background(Color ("SilverCol")
+                         
+                         )
+                     
+                     })
+                     
+                     
+                     Button(action:{
+                     }, label: {
+                         Text("3")
+                             .foregroundColor(.white)
+                             .font(.system(size: 40 ))
+                             .fontWeight(.ultraLight)
+                             .frame(width:
+                                 UIScreen.main.bounds.width/4, height:UIScreen.main.bounds.width/4, alignment: .center).background(Color ("SilverCol")
+                         
+                         )
+                     
+                     })
+                     
+                    Button(action:{
+                    }, label: {
+                        Text("+")
+                            .foregroundColor(.white)
+                            .font(.system(size: 40 ))
+                            .fontWeight(.ultraLight)
+                            .frame(width:
+                                UIScreen.main.bounds.width/4, height:UIScreen.main.bounds.width/4, alignment: .center).background(Color ("OrangCol")
+                        
+                        )
+                    
+                    })
+
+                     //--конец ряда
+                 }
+                 // --
+     
+                //---Гор стек 5
+                 HStack(spacing: 1) {
+                     
+                     //---Button
+                     Button(action:{
+                     }, label: {
+                         Text("0")
+                             .foregroundColor(.white)
+                             .font(.system(size: 40 ))
+                             .fontWeight(.ultraLight)
+                             .frame(width:
+                                 UIScreen.main.bounds.width/2, height:UIScreen.main.bounds.width/4, alignment: .center).background(Color ("SilverCol")
+                         
+                         )
+                     
+                     })
+                     
+   
+                     Button(action:{
+                     }, label: {
+                         Text(",")
+                      //   Image(systemName: "devide")
+                             .foregroundColor(.white)
+                             .font(.system(size: 40 ))
+                             .fontWeight(.ultraLight)
+                             .frame(width:
+                                 UIScreen.main.bounds.width/4, height:UIScreen.main.bounds.width/4, alignment: .center).background(Color ("SilverCol")
+                         
+                         )
+                     
+                     })
+                    
+               
+                     Button(action:{
+                     }, label: {
+                         Text("=")
+                             .foregroundColor(.white)
+                             .font(.system(size: 40 ))
+                             .fontWeight(.ultraLight)
+                             .frame(width:
+                                 UIScreen.main.bounds.width/4, height:UIScreen.main.bounds.width/4, alignment: .center).background(Color ("OrangCol")
+                         
+                         )
+                     
+                     })
+         
+                    
+                     //--конец ряда
+                 }
+                 // --
+                
+            }.frame(width: UIScreen.main.bounds.width, alignment: .trailing)
+        }.ignoresSafeArea()
+        
     }
 }
 
